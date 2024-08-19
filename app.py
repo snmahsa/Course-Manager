e075ed923ac0648536257a0fdf6568f8d29dc007
mahsa <mahsasanaei.n@gmail.com>
2024-08-19 14:53:52 +0330

    Add route for 'profile'

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
