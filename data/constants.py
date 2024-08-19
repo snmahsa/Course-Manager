073212496c8689d7343c667176d78563915627ae
mahsa <mahsasanaei.n@gmail.com>
2024-08-19 17:09:06 +0330

    Add constants.py in data folder
    this file has dictionary and list

---
+++ data/constants.py
@@ -0,0 +1,22 @@
+student_info = {
+    'name': 'Mahsa Sanaei',
+    'email': 'Mahsasanaei.n@gmail.com',
+    'major': 'Computer Engineer'
+}
+
+
+
+courses_list = [
+    {
+        'id': 1,
+        'name': 'Machine Learning',
+        'description': 'Learning Machine Leaning',
+        'teacher':'Jadi'
+    },
+    {
+        'id': 2,
+        'name': 'Deep Learning',
+        'description': 'Learning MachDeepne Leaning',
+        'teacher':'Sanaei'
+    }
+]
